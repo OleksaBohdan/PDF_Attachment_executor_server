d6797687fcc49b1ecca19fa2977017b811c9d0f6
OleksaBohdan <bohdan.oleksa@gmail.com>
2022-07-12 13:04:28 +0200

    Added error handling

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const extractXmlFromPdf = require('./extractor');
 const config = require('./config');
 const logger = require('./logger');
 
+let lastExtractedTime;
+let lastExtractedFile;
+
 try {
   http
     .createServer((request, response) => {
@@ -13,7 +16,7 @@ try {
         const fileName = fileNameWithExt.split('.')[0];
         const fileExt = fileNameWithExt.split('.')[1];
         let body = [];
-        logger.info(`Server got file ${fileNameWithExt}`);
+        logger.info(`Server got file: ${fileNameWithExt}`);
 
         if (request.headers['content-length'] == 0 || !fileNameWithExt || fileExt !== 'pdf') {
           logger.error('File is not available.');
@@ -28,33 +31,38 @@ try {
               fs.writeFileSync(path.join(__dirname, './PDFs/', `${fileName}.pdf`), Buffer.concat(body));
             } catch (error) {
               logger.error(error.message);
-              console.log(error.message);
               response.statusCode = 500;
-              response.end(`Error while reading PDF file.\n ${error.message}`);
+              response.end(`Error while reading PDF file:\n${error.message}`);
             }
-            try {
-              extractXmlFromPdf(`${fileName}.pdf`, () => {
-                const xml = fs.readFileSync(path.join(__dirname, './XMLs', `${fileName}.xml`));
-                response.write(xml);
-                response.end();
-                fs.unlink(path.join(__dirname, './PDFs', `${fileName}.pdf`), () => {});
-                fs.unlink(path.join(__dirname, './XMLs', `${fileName}.xml`), () => {});
-              });
-            } catch (error) {
-              logger.error(error.message);
-              console.log(error.message);
+            extractXmlFromPdf(`${fileName}.pdf`, () => {
+              lastExtractedTime = new Date(Date.now());
+              lastExtractedFile = fileNameWithExt;
+              const xml = fs.readFileSync(path.join(__dirname, './XMLs', `${fileName}.xml`));
+              response.end(xml);
+              fs.unlink(path.join(__dirname, './PDFs', `${fileName}.pdf`), () => {});
+              fs.unlink(path.join(__dirname, './XMLs', `${fileName}.xml`), () => {});
+            }).catch((error) => {
               response.statusCode = 500;
-              response.end(`Error while extracting XML from PDF file.\n ${error.message}`);
-            }
+              response.end(`Error while extracting XML from PDF file:\n${error}`);
+            });
           });
         }
+
+        request.on('aborted', (error) => {
+          fs.unlink(path.join(__dirname, './XMLs/', `${fileName}.xml`), (error) => {
+            if (error) throw error;
+          });
+          response.statusCode = 500;
+          response.end(error.message);
+        });
       } else {
         response.statusCode = 200;
-        response.end('Server working: OK');
+        response.end(
+          `Server working: OK\nlastExtractedTime: ${lastExtractedTime}\nlastExtractedFile: ${lastExtractedFile}`
+        );
       }
     })
-    .listen(config.PORT, () => console.log(`Server started on port ${config.PORT}`));
+    .listen(config.PORT, () => console.log(`Server started on port: ${config.PORT}`));
 } catch (error) {
   logger.error(error.message);
-  console.log(error.message);
 }
--- extractor.js
+++ extractor.js
@@ -2,30 +2,37 @@
 const pdfjs = require('pdfjs-dist/legacy/build/pdf.js');
 const path = require('path');
 const fs = require('fs');
-const logger = require('pino');
+const logger = require('./logger');
 
 module.exports = function extractXmlFromPdf(filename, cb) {
-  const fileName = filename;
+  return new Promise(function (resolve, reject) {
+    const fileName = filename;
+    const filePath = path.join(__dirname, './PDFs/', fileName);
+    const filenameWithExt = filePath.replace(/^.*[\\\/]/, ''); // - FileName with extension
+    const fileNameArr = filenameWithExt.split('.');
+    const nameWithoutExt = fileNameArr[0]; // - FileName without extension
 
-  const filePath = path.join(__dirname, './PDFs/', fileName);
+    const getAttachment = async function (src) {
+      const doc = await pdfjs.getDocument(src).promise;
+      return await doc.getAttachments();
+    };
 
-  const filenameWithExt = filePath.replace(/^.*[\\\/]/, ''); // - FileName with extension
-  const fileNameArr = filenameWithExt.split('.');
-  const nameWithoutExt = fileNameArr[0]; // - FileName without extension
+    const createXML = async function (src) {
+      let attachment;
+      try {
+        attachment = await getAttachment(src);
+        const unit8 = attachment['ZUGFeRD-invoice.xml']['content'];
+        const str = new TextDecoder('utf-8').decode(unit8);
+        fs.writeFileSync(path.join(__dirname, './XMLs/', `${nameWithoutExt}.xml`), str, 'utf8');
+        cb();
+        resolve();
+        return;
+      } catch (error) {
+        logger.error(error.message);
+        reject(error);
+      }
+    };
 
-  const getAttachment = async function (src) {
-    const doc = await pdfjs.getDocument(src).promise;
-    return await doc.getAttachments();
-  };
-
-  const createXML = async function (src) {
-    const attachment = await getAttachment(src);
-    const unit8 = attachment['ZUGFeRD-invoice.xml']['content'];
-    const str = new TextDecoder('utf-8').decode(unit8);
-    fs.writeFileSync(path.join(__dirname, './XMLs/', `${nameWithoutExt}.xml`), str, 'utf8');
-    cb();
-    return;
-  };
-
-  createXML(filePath);
+    createXML(filePath);
+  });
 };
